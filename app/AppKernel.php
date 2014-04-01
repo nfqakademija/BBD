792e63a17bddaae975c24a8ebbc2fdf7fca116d4
Simonas Šerlinskas <simonas.serlinskas@nfq.com>
2014-04-01 17:22:02 +0300

    removed acme bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,10 +22,10 @@ class AppKernel extends Kernel
 
             #Third party components
             new FOS\UserBundle\FOSUserBundle(),
+            new NFQAkademija\RecipesBundle\NFQAkademijaRecipesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();

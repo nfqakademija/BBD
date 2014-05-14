b100e4dd569685b836759ca5faec454a98c144d9
svamak <svajunas.maksvytis@gmail.com>
2014-05-14 15:49:43 +0300

    added comments to admin zone

--- src/NFQAkademija/RecipesBundle/Controller/AdminController.php
+++ src/NFQAkademija/RecipesBundle/Controller/AdminController.php
@@ -12,7 +12,29 @@ class AdminController extends Controller
         $em = $this->getDoctrine()->getManager();
 
         switch($type){
-            case "users": return $this->render('NFQAkademijaRecipesBundle:Admin:users.html.twig',array());break;
+            case "users":
+                /*
+                $users = "";
+                $users_data = $em->getRepository('NFQAkademijaBaseBundle:User')->findAll();
+                foreach($users_data as $user){
+                    $text = $fact->getText();
+                    $id = $fact->getId();
+                    $users .=
+                        "<div id='user_$id'>
+                        <div class='input_title'>$id</div>
+                        <input type='text' class='new_recipe_input' placeholder='Faktas' value='$text'/>
+                        <div class='sub_save' onclick=\"save_fact('$id')\"></div>
+                        <div class='sub_delete' onclick=\"delete_fact('$id')\"></div>
+                    </div>";
+                }
+                */
+                $users = "";
+                return $this->render('NFQAkademijaRecipesBundle:Admin:users.html.twig',
+                    array(
+                        'users' => $users,
+                    ));
+                break;
+
             case "locations": return $this->render('NFQAkademijaRecipesBundle:Admin:locations.html.twig',array());break;
 
             case "facts":
@@ -39,24 +61,29 @@ class AdminController extends Controller
             case "recipes": return $this->render('NFQAkademijaRecipesBundle:Admin:recipes.html.twig',array());break;
             case "filters": return $this->render('NFQAkademijaRecipesBundle:Admin:filters.html.twig',array());break;
             case "ingredients": return $this->render('NFQAkademijaRecipesBundle:Admin:ingredients.html.twig',array());break;
+
             case "comments":
                 $comments = "";
                 $comments_data = $em->getRepository('NFQAkademijaBaseBundle:Comment')->findAll();
                 foreach($comments_data as $comment){
                     $text = $comment->getText();
                     $id = $comment->getId();
-
+                    $user = "Autorius";//$comment->getUser()->getName();
                     $comments .=
                         "<div id='comment_$id'>
-                        <div class='input_title'>$id</div>
+                        <div class='input_title'>$user</div>
                         <input type='text' class='new_recipe_input' placeholder='Komentaras' value='$text'/>
                         <div class='sub_save' onclick=\"save_comment('$id')\"></div>
                         <div class='sub_delete' onclick=\"delete_comment('$id')\"></div>
                     </div>";
                 }
 
-                return $this->render('NFQAkademijaRecipesBundle:Admin:comments.html.twig',array());
+                return $this->render('NFQAkademijaRecipesBundle:Admin:comments.html.twig',
+                    array(
+                        'comments' => $comments,
+                    ));
                 break;
+
             case "options": return $this->render('NFQAkademijaRecipesBundle:Admin:options.html.twig',array());break;
             default: return $this->render('NFQAkademijaRecipesBundle:Admin:users.html.twig',array());break;
         }
